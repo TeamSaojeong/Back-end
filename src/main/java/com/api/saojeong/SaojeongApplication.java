1a3c236ecd760abf65649c2c89c421e823674b13
LgE02 <leegaeun023@gmail.com>
2025-08-14 00:31:46 +0900

    :sparkles: 생성일, 수정일 어노테이션 추가

--- src/main/java/com/api/saojeong/SaojeongApplication.java
+++ src/main/java/com/api/saojeong/SaojeongApplication.java
@@ -2,8 +2,10 @@ package com.api.saojeong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SaojeongApplication {
 
 	public static void main(String[] args) {
