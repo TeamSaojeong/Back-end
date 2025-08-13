25bc4ddc549be4201ac2386a6443b10a31f3d2cf
LgE02 <leegaeun023@gmail.com>
2025-08-14 00:27:03 +0900

    :sparkles: S3 에외 처리

---
+++ src/main/java/com/api/saojeong/Parking/exception/ParkingErrorCode.java
@@ -0,0 +1,19 @@
+package com.api.saojeong.Parking.exception;
+
+import com.api.saojeong.global.utill.init.ErrorCode;
+import com.api.saojeong.global.utill.response.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ParkingErrorCode implements BaseResponseCode {
+
+    S3_UPLOAD_FAILED("S3_UPLOAD_FAILED", 500, "S3 업로드 중 오류가 발생했습니다.");
+
+
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+
+}
--- src/main/java/com/api/saojeong/global/utill/exception/CustomException.java
+++ src/main/java/com/api/saojeong/global/utill/exception/CustomException.java
@@ -2,11 +2,20 @@ package com.api.saojeong.global.utill.exception;
 
 
 import com.api.saojeong.global.utill.init.ErrorCode;
+import com.api.saojeong.global.utill.response.BaseResponseCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
-    private final ErrorCode errorCode;
+    private final Object errorCode;
+
+    // 기존 ErrorCode용 생성자
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(BaseResponseCode errorCode) {
+        this.errorCode = errorCode;
+    }
 }
