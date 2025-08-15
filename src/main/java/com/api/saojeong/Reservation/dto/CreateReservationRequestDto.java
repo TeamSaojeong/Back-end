166dc887f3b5ff2de1e842d0bf795c35aed79565
LgE02 <leegaeun023@gmail.com>
2025-08-15 15:33:28 +0900

    :recycle: 남은 시간 변수명 변경

--- src/main/java/com/api/saojeong/Reservation/dto/CreateReservationRequestDto.java
+++ src/main/java/com/api/saojeong/Reservation/dto/CreateReservationRequestDto.java
@@ -4,5 +4,5 @@ import lombok.Getter;
 
 @Getter
 public class CreateReservationRequestDto {
-    private int useMinutes;
+    private int usingMinutes;
 }
--- src/main/java/com/api/saojeong/Reservation/dto/CreateReservationResponseDto.java
+++ src/main/java/com/api/saojeong/Reservation/dto/CreateReservationResponseDto.java
@@ -5,12 +5,13 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalTime;
 
 public record CreateReservationResponseDto(
+        Long reservationId,
         Long memberId,
         String parkName,
         @JsonFormat(pattern = "HH:mm")
         LocalTime start,
         @JsonFormat(pattern = "HH:mm")
         LocalTime end,
-        int duration
+        int usingMinutes
 ) {
 }
